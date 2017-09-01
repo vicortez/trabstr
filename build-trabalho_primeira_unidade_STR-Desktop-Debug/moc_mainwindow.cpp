ae509a273a6a37dab22ac2f91e24289d1fcaa029
paulovictorcorreia <paulocorreiaufrn@gmail.com>
2017-09-01 13:34:17 -0300

    isso é apenas mais um teste

--- build-trabalho_primeira_unidade_STR-Desktop-Debug/moc_mainwindow.cpp
+++ build-trabalho_primeira_unidade_STR-Desktop-Debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../trabalho_primeira_unidade_STR/mainwindow.h"
+#include "../../GIT/Projeto_STR_Unidade1/trabalho_primeira_unidade_STR/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
